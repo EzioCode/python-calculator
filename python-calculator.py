faad0a11ed48e95dc4114bc5fa960730c1771716
unknown <smurfka3@gmail.com>
2020-06-26 22:52:53 -0300

    python-calculator

---
+++ python-calculator.py
@@ -0,0 +1,32 @@
+import re
+
+print('Our Magical Calculator')
+print("Type 'quit' to exit \n")
+
+previous = 0
+run = True
+
+def Math():
+  global previous
+  global run
+  equation = ""
+
+  if previous == 0:
+    equation = input("Enter equation: ")
+  else : 
+    equation = input(str(previous))
+  
+  if equation == 'quit':
+    print("Goodbye, human")
+    run = False
+    
+  else:
+    equation = re.sub('[a-zA-Z,.:()" ]]', '', equation)
+    
+    if previous == 0:
+      previous = eval(equation)
+    else:
+      previous = eval(str(previous) + equation)
+    
+while run:
+  Math()
